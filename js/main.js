070251aec61bc7a3174617d5ee28f7ef81764434
William Bolton <wbolton@fandm.edu>
2014-07-02 15:04:05 -0400

    Late afternoon Wednesday changes

--- js/main.js
+++ js/main.js
@@ -5,24 +5,28 @@ function init() {
 		printButton.addEventListener("click", laserPrinter.print);
 	}
 
+//var toner = document.getElementById("tonerLevel").value;
+
 var laserPrinter = {
     password: "cats",
+    //toner: document.getElementById("tonerLevel").value,
+    toner: 5,
   	print: function print() {
-  		var self = this;
-  		var userToner = document.getElementById("tonerLevel").value;
+  		var toner = document.getElementById("tonerLevel").value;
 		if (!document.getElementById("onButton").checked) {
 			document.getElementById("printerMessages").innerHTML = ("You cannot print because the printer is off.");
 			return;
 		} else if (document.getElementById("wifiPassword").value !== laserPrinter.password) {
 			document.getElementById("printerMessages").innerHTML = ("You cannot print because you are not connected to WiFi.");
 			return;
-		} else if (userToner == 0) {
+		} else if (toner == 0) {
 			document.getElementById("printerMessages").innerHTML = ("You cannot print because you are out of toner.");
 			return;
 		} else {
 			document.getElementById("printerMessages").innerHTML = ("You print your document.");
-			userToner--;
-		    document.getElementById("printerSupplies").innerHTML = ("The printer has " + userToner + " units of toner left.");
+			toner--;
+		    document.getElementById("printerSupplies").innerHTML = ("The printer has " + toner + " units of toner left.");
+		    return;
 		}
 	},
 };
